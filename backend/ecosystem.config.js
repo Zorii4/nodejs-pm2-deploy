0789594e87f1040ae2df22f08113b697547e7155
Anton Zorin <Zori44@yandex.ru>
2026-07-09 12:39:27 +0300

    use env.deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,18 +1,24 @@
+require('dotenv').config({ path: '.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
+} = process.env;
+
 module.exports = {
   apps: [{
     name: 'mesto-backend',
-    script: '/home/zori4/backend/source/backend/src/app.ts',
-    interpreter: '/home/zori4/backend/source/backend/node_modules/.bin/ts-node',
+    script: `${DEPLOY_PATH}/source/backend/src/app.ts`,
+    interpreter: `${DEPLOY_PATH}/source/backend/node_modules/.bin/ts-node`,
   }],
   deploy: {
     production: {
-      user: 'zori4',
-      host: '84.252.136.65',
-      ref: 'origin/master',
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
       repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
-      path: '/home/zori4/backend',
-      'pre-deploy-local': 'scp ./.env zori4@84.252.136.65:/home/zori4/backend/source/backend',
-      'post-deploy': 'cd /home/zori4/backend/source/backend && npm i && pm2 start ecosystem.config.js --env production',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && pm2 start ecosystem.config.js --env production`,
     },
   },
 };
