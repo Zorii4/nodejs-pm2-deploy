b463f1ff43e425912aa9834b8d44562c2c100495
Anton Zorin <Zori44@yandex.ru>
2026-07-09 11:45:09 +0300

    hardcoded

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
       path: '/home/zori4/backend',
       'pre-deploy-local': 'scp ./.env zori4@84.252.136.65:/home/zori4/backend/source/backend',
-      'post-deploy': 'cd /home/zori4/backend/source/backend && npm i && pm2 delete all && pm2 start ecosystem.config.js --env production',
+      'post-deploy': 'cd /home/zori4/backend/source/backend && npm i && pm2 start ecosystem.config.js --env production',
     },
   },
 };
