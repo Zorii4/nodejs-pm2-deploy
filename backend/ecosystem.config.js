d64420190bcf07e91bb940a113b4c3f9888b02de
Anton Zorin <Zori44@yandex.ru>
2026-07-09 10:18:30 +0300

    add deploy config

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: '.env.deploy' });
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF } = process.env;
+
+module.exports = {
+  apps: [{ name: 'mesto-backend', script: './app.js' }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source`,
+      'post-deploy': 'npm i && pm2 restart ecosystem.config.js --env production',
+    },
+  },
+};
