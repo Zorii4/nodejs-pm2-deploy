59d5718bcb4b08342dc94cd8433440ec837ab98c
Anton Zorin <Zori44@yandex.ru>
2026-07-09 13:12:36 +0300

    use env.deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -8,8 +8,8 @@ const {
 module.exports = {
   apps: [{
     name: 'mesto-backend',
-    script: `${DEPLOY_PATH}/src/app.ts`,
-    interpreter: `${DEPLOY_PATH}/node_modules/.bin/ts-node`,
+    script: `${DEPLOY_PATH}/source/backend/src/app.ts`,
+    interpreter: `${DEPLOY_PATH}/source/backend/node_modules/.bin/ts-node`,
   }],
   deploy: {
     production: {
@@ -18,8 +18,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': `cd ${DEPLOY_PATH} && npm i && pm2 start ecosystem.config.js --env production`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && pm2 start ecosystem.config.js --env production`,
     },
   },
 };
