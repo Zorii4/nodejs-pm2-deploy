93d830efd97dc3555873a5875d98928bd5a90a49
Anton Zorin <Zori44@yandex.ru>
2026-07-09 16:53:29 +0300

    Configure pm2 deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,25 +1,49 @@
 const path = require('path');
+
 require('dotenv').config({ path: path.resolve(__dirname, '.env.deploy') });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF,
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
-  apps: [{
-    name: 'mesto-backend',
-    script: `${DEPLOY_PATH}/source/backend/src/app.ts`,
-    interpreter: `${DEPLOY_PATH}/source/backend/node_modules/.bin/ts-node`,
-  }],
+  apps: [
+    {
+      name: 'mesto-backend',
+      cwd: `${DEPLOY_PATH}/current/backend`,
+      script: 'src/app.ts',
+      interpreter: './node_modules/.bin/ts-node',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && pm2 start ecosystem.config.js --env production`,
+
+      'pre-deploy-local': `
+        ssh ${DEPLOY_USER}@${DEPLOY_HOST} "mkdir -p ${DEPLOY_PATH}/shared" &&
+        scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env &&
+        scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/shared/.env.deploy
+      `,
+
+      'post-deploy': `
+        cd ${DEPLOY_PATH}/current/backend &&
+        cp ${DEPLOY_PATH}/shared/.env .env &&
+        cp ${DEPLOY_PATH}/shared/.env.deploy .env.deploy &&
+        npm i &&
+        pm2 startOrReload ecosystem.config.js --env production
+      `,
     },
   },
 };
--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,16 +1,29 @@
 require('dotenv').config({ path: '.env.deploy' });
-const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF } = process.env;
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO,
+} = process.env;
 
 module.exports = {
   apps: [],
+
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
+      repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd /home/zori4/frontend/source/frontend && npm i && NODE_OPTIONS=--openssl-legacy-provider npm run build',
+
+      'post-deploy': `
+        cd ${DEPLOY_PATH}/current/frontend &&
+        npm i &&
+        NODE_OPTIONS=--openssl-legacy-provider npm run build
+      `,
     },
   },
 };
