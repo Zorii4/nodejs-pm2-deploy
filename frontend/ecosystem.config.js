208bc8b3c3bad7f66c293e3fc581d25fd092fa8a
Anton Zorin <Zori44@yandex.ru>
2026-07-09 11:14:16 +0300

    fix

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: '.env.deploy' });
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF } = process.env;
+
+module.exports = {
+  apps: [],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && npm i && npm run build',
+    },
+  },
+};
