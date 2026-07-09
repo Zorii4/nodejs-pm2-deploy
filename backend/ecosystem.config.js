c71c7e90e1d25e180255252a1c154691bf327018
Anton Zorin <Zori44@yandex.ru>
2026-07-09 13:14:38 +0300

    use env.deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/Zorii4/nodejs-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend && scp ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && pm2 start ecosystem.config.js --env production`,
     },
   },
