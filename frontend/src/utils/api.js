b675bbbff6c703737f577476173dfc95a348a372
Anton Zorin <Zori44@yandex.ru>
2026-07-09 12:01:57 +0300

    fix api url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,7 +139,7 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('http://localhost:3000');
+  const api = new Api('http://84.252.136.65:3000');
   
   export default api;
   
